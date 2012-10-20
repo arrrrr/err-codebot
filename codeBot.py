87ff100c22bd6770fb57f0d47b95ccec12b6ecef
Sebastian Ullrich <sebasti@nullri.ch>
2012-10-20 22:45:09 +0200

    Add Haskell support for expressions and modules

--- codeBot.py
+++ codeBot.py
@@ -1,5 +1,6 @@
 __author__ = 'gbin'
 
+import requests
 from errbot.botplugin import BotPlugin
 from errbot import botcmd
 from codepad import CodePad
@@ -20,3 +21,17 @@ class CodeBot(BotPlugin):
     def cpp(self, mess, args):
         """ Execute the c expression """
         return CodePad(args, lang='C++').eval()
+
+    @botcmd
+    def hs(self, mess, args):
+        """ Evaluate the Haskell expression in ghci (tryhaskell.org) """
+        r = requests.get("http://tryhaskell.org/haskell.json", params={'method':'eval', 'expr':args}).json
+        if r.has_key('result'):
+            return r['result'] + " :: " + r['type']
+        else:
+            return r['error']
+
+    @botcmd
+    def haskell(self, mess, args):
+        """ Execute the Haskell module """
+        return CodePad(args, lang='Haskell').eval()
